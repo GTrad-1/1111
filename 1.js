46a68c250e186a605f5b1c0e0676015aca6fd028
GTrad-1 <103395331+GTrad-1@users.noreply.github.com>
2022-04-19 16:15:08 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,16 @@
+function total(){
+var N1=document.getElementById("N1").value;
+var N2=document.getElementById("N2").value;
+var total=Number(N1)+Number(N2);
+document.getElementById("result").innerHTML = total;
+{
+var N1=document.getElementById("N1").value;
+if(isNaN(N1) || isNaN(N2))
+{ alert("Please enter a number");
+
+return false;}
+} result.value = func(N1,N2);
+}
+function add(N1,N2){
+return N1+N2;
+}
